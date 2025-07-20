2b5a4e042ff245b344fee2099006456522321f31
Leandro Fonseca <leandromkt@outlook.com>
2025-07-19 23:34:09 -0300

    tratamento de erro de status code

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -4,6 +4,7 @@ import { serviceListEpisodes } from "../services/list-episodes-service";
 import { serviceFilterEpisodes } from "../services/filter-episodes-service";
 import { StatusCode } from "../utils/status-code";
 import { ContentType } from "../utils/content-type";
+import { FilterPodcastModel } from "../models/filter-podcast-model";
 
 export const getListEpisodes = async (
     req: IncomingMessage,
@@ -20,8 +21,8 @@ export const getFilterEpisodes = async (
     res: ServerResponse
 ) => {
 
-    const content = await serviceFilterEpisodes(req.url);
+    const content: FilterPodcastModel = await serviceFilterEpisodes(req.url);
 
-    res.writeHead(StatusCode.OK, { "Content-Type": ContentType.JSON });
-    res.end(JSON.stringify(content));
+    res.writeHead(content.statusCode, { "Content-Type": ContentType.JSON });
+    res.end(JSON.stringify(content.body));
 }
--- src/services/filter-episodes-service.ts
+++ src/services/filter-episodes-service.ts
@@ -1,14 +1,25 @@
 import { IncomingMessage } from "http";
 import { repositoryPodcast } from "../repositories/podcasts-repository";
+import { FilterPodcastModel } from "../models/filter-podcast-model";
+import { StatusCode } from "../utils/status-code";
 
 
 export const serviceFilterEpisodes = async (
     podcastName: string | undefined
-) => {
+): Promise<FilterPodcastModel> => {
+
+    let responseFormat: FilterPodcastModel = {
+        statusCode: 0,
+        body: []
+    };
 
     const queryString = podcastName?.split("?p=")[1] || "";
 
     const data = await repositoryPodcast(queryString);
 
-    return data;
+    responseFormat.statusCode = data.length !== 0 ? StatusCode.OK : StatusCode.NoContent;
+
+    responseFormat.body = data;
+
+    return responseFormat;
 }
